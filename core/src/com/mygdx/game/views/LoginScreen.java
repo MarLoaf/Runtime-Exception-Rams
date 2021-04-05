3c740ffafc113ccdc9b731f44a48b378a52f0944
Pchel666 <pchel66694@gmail.com>
2021-04-04 21:47:48 -0400

    added a test custom skin with our font

--- core/src/com/mygdx/game/views/LoginScreen.java
+++ core/src/com/mygdx/game/views/LoginScreen.java
@@ -2,11 +2,19 @@ package com.mygdx.game.views;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.Hinting;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.game.Tutor;
 
@@ -23,10 +31,54 @@ public class LoginScreen implements Screen {
 		stage.draw();
 		Table table = new Table();
 		table.setFillParent(true);
-		table.setDebug(true);
+		//table.setDebug(true); remove // for guides around table elements (red lines)
 		stage.addActor(table);
-		Skin skin = new Skin(Gdx.files.internal("skin/clean-crispy-ui.json"));
-		TextButton login = new TextButton("Login", skin);
+		Skin skin = new Skin(Gdx.files.internal("skin/test.json")) {
+            //Override json loader to process FreeType fonts from skin JSON
+            @Override
+            protected Json getJsonLoader(final FileHandle skinFile) {
+                Json json = super.getJsonLoader(skinFile);
+                final Skin skin = this;
+
+                json.setSerializer(FreeTypeFontGenerator.class, new Json.ReadOnlySerializer<FreeTypeFontGenerator>() {
+                    @Override
+                    public FreeTypeFontGenerator read(Json json,
+                            JsonValue jsonData, Class type) {
+                        String path = json.readValue("font", String.class, jsonData);
+                        jsonData.remove("font");
+
+                        Hinting hinting = Hinting.valueOf(json.readValue("hinting", 
+                                String.class, "AutoMedium", jsonData));
+                        jsonData.remove("hinting");
+
+                        TextureFilter minFilter = TextureFilter.valueOf(
+                                json.readValue("minFilter", String.class, "Nearest", jsonData));
+                        jsonData.remove("minFilter");
+
+                        TextureFilter magFilter = TextureFilter.valueOf(
+                                json.readValue("magFilter", String.class, "Nearest", jsonData));
+                        jsonData.remove("magFilter");
+
+                        FreeTypeFontParameter parameter = json.readValue(FreeTypeFontParameter.class, jsonData);
+                        parameter.hinting = hinting;
+                        parameter.minFilter = minFilter;
+                        parameter.magFilter = magFilter;
+                        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(skinFile.parent().child(path));
+                        BitmapFont font = generator.generateFont(parameter);
+                        skin.add(jsonData.name, font);
+                        if (parameter.incremental) {
+                            generator.dispose();
+                            return null;
+                        } else {
+                            return generator;
+                        }
+                    }
+                });
+
+                return json;
+            }
+        };
+		ImageTextButton login = new ImageTextButton("Login", skin);
 		table.add(login).fillX().uniformX();
 		table.row().pad(10, 0, 10, 0);
 	}
