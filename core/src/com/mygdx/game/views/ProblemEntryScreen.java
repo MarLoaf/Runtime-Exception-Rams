ad01919ea6d5a5cf644a6a693fa36beb0ed83858
Pchel666 <pchel66694@gmail.com>
2021-04-05 22:17:45 -0400

    able to go back from all the screens now

--- core/src/com/mygdx/game/views/ProblemEntryScreen.java
+++ core/src/com/mygdx/game/views/ProblemEntryScreen.java
@@ -11,12 +11,15 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.Hinting;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -93,11 +96,21 @@ public class ProblemEntryScreen implements Screen {
 		//creating actors
         Label description = new Label("You're about to begin a set of practice problems, click Begin to start the problems", skin);
 		ImageTextButton begin = new ImageTextButton("Begin", skin, "green");
+		ImageTextButton back = new ImageTextButton("Back", skin, "pink");
         //layout
 		table.row();
+		table.add(back);
+		table.row().pad(10, 0, 0, 0);
 		table.add(description).fillX().uniformX();
 		table.row().pad(10, 0, 0, 0);
 		table.add(begin);
+		//adding button functionality
+		back.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				parent.changeScreen(Tutor.HOME);
+			}
+		});
 	}
 
 	@Override
