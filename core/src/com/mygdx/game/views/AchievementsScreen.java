cfa323f9935ff4926dec4c399e6ea24a63828686
Luther <15237257+LutherEvans@users.noreply.github.com>
2021-04-07 18:12:23 -0400

    Update AchievementsScreen.java

--- core/src/com/mygdx/game/views/AchievementsScreen.java
+++ core/src/com/mygdx/game/views/AchievementsScreen.java
@@ -95,65 +95,65 @@ public class AchievementsScreen implements Screen {
 //=======
       
 		//making tables
-				Table table = new Table();
-				table.setFillParent(true);
-				stage.addActor(table);
-		        //creating different buttons/textfields/labels
-				ImageTextButton back = new ImageTextButton("Back", skin, "pink");
-				//layout:
-				table.add(back);
-				//
-				//Button logout = new Button(skin, "Exit");
-				Image Iron1 = new Image(new Texture(Gdx.files.internal("images/iron.png")));
-				Image AdditionIron = new Image(new Texture(Gdx.files.internal("images/iron.png")));
-				Image AdditionDiamond = new Image(new Texture(Gdx.files.internal("images/diamond.png")));
-				Image AdditionRuby = new Image(new Texture(Gdx.files.internal("images/ruby.png")));
-				Image Ruby = new Image(new Texture(Gdx.files.internal("images/ruby.png")));
-				Image Diamond = new Image(new Texture(Gdx.files.internal("images/diamond.png")));
-				Image Iron = new Image(new Texture(Gdx.files.internal("images/iron.png")));
-				Label achievement1 = new Label("Addition", skin, "noBackground");
-				Label achievement2 = new Label("Subtraction", skin, "noBackground");
-				Label achievement3 = new Label("Division", skin, "noBackground");
-			
-				ImageTextButton achiemeventsButton = new ImageTextButton("My Achievements", skin);
-				//layout:
-				table.top();
-				table.row();
-				table.add(achiemeventsButton).fillX().uniformX().pad(5).padBottom(100).width(Gdx.graphics.getWidth()/5);
-				table.add();
-				table.row();
-				table.row();
-				table.add();
-				table.add(Iron1).pad(5).align(Align.right);
-				table.add(Ruby).pad(5).align(Align.right);
-				table.add(achievement1).pad(5).width(Gdx.graphics.getWidth()/5).align(Align.left);
-				table.row();
-				/*if (parent.additionAchievement == 1) {
-					table.add(AdditionIron).pad(5).align(Align.right);
-					table.add(addition).pad(5).width(Gdx.graphics.getWidth()/5).align(Align.right);
-				} else if (parent.additionAchievement == 2) {
-					table.add(AdditionDiamond).pad(5).align(Align.right);
-					table.add(addition).pad(5).width(Gdx.graphics.getWidth()/5).align(Align.right);
-				} else if (parent.additionAchievement == 3) {
-					table.add(AdditionRuby).pad(5).align(Align.right);
-					table.add(addition).pad(5).width(Gdx.graphics.getWidth()/5).align(Align.right);
-				}else {
-					table.add();
-					table.add();
-				}
-				*/
-				table.add(Diamond).pad(5).align(Align.right);
-				table.add(achievement2).pad(5).width(Gdx.graphics.getWidth()/5).align(Align.left);
-				table.row();
-				table.add();
-				table.add();
-				table.add(Iron).pad(5).align(Align.right);
-				table.add(achievement3).pad(5).width(Gdx.graphics.getWidth()/5).align(Align.left);
-				//adding button functionality
-				
-				
-//>>>>>>> Stashed changes
+		Table table = new Table();
+		table.setFillParent(true);
+		stage.addActor(table);
+        //creating different buttons/textfields/labels
+		ImageTextButton back = new ImageTextButton("Back", skin, "pink");
+		//layout:
+		table.add(back);
+		//
+		//Button logout = new Button(skin, "Exit");
+		Image Iron1 = new Image(new Texture(Gdx.files.internal("images/iron.png")));
+		Image AdditionIron = new Image(new Texture(Gdx.files.internal("images/iron.png")));
+		Image AdditionDiamond = new Image(new Texture(Gdx.files.internal("images/diamond.png")));
+		Image AdditionRuby = new Image(new Texture(Gdx.files.internal("images/ruby.png")));
+		Image Ruby = new Image(new Texture(Gdx.files.internal("images/ruby.png")));
+		Image Diamond = new Image(new Texture(Gdx.files.internal("images/diamond.png")));
+		Image Iron = new Image(new Texture(Gdx.files.internal("images/iron.png")));
+		Label achievement1 = new Label("Addition", skin, "noBackground");
+		Label achievement2 = new Label("Subtraction", skin, "noBackground");
+		Label achievement3 = new Label("Division", skin, "noBackground");
+	
+		ImageTextButton achiemeventsButton = new ImageTextButton("My Achievements", skin);
+		//layout:
+		table.top();
+		table.row();
+		table.add(achiemeventsButton).fillX().uniformX().pad(5).padBottom(100).width(Gdx.graphics.getWidth()/5);
+		table.add();
+		table.row();
+		table.row();
+		table.add();
+		table.add(Iron1).pad(5).align(Align.right);
+		table.add(Ruby).pad(5).align(Align.right);
+		table.add(achievement1).pad(5).width(Gdx.graphics.getWidth()/5).align(Align.left);
+		table.row();
+		/*if (parent.additionAchievement == 1) {
+			table.add(AdditionIron).pad(5).align(Align.right);
+			table.add(addition).pad(5).width(Gdx.graphics.getWidth()/5).align(Align.right);
+		} else if (parent.additionAchievement == 2) {
+			table.add(AdditionDiamond).pad(5).align(Align.right);
+			table.add(addition).pad(5).width(Gdx.graphics.getWidth()/5).align(Align.right);
+		} else if (parent.additionAchievement == 3) {
+			table.add(AdditionRuby).pad(5).align(Align.right);
+			table.add(addition).pad(5).width(Gdx.graphics.getWidth()/5).align(Align.right);
+		}else {
+			table.add();
+			table.add();
+		}
+		*/
+		table.add(Diamond).pad(5).align(Align.right);
+		table.add(achievement2).pad(5).width(Gdx.graphics.getWidth()/5).align(Align.left);
+		table.row();
+		table.add();
+		table.add();
+		table.add(Iron).pad(5).align(Align.right);
+		table.add(achievement3).pad(5).width(Gdx.graphics.getWidth()/5).align(Align.left);
 		//adding button functionality
+		
+		
+//>>>>>>> Stashed changes
+//adding button functionality
 		back.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor) {
