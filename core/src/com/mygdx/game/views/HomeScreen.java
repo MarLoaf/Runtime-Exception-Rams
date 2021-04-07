31f9bebc0ada7338ea11edbceb0bd468755254c5
Pchel666 <pchel66694@gmail.com>
2021-04-07 16:12:52 -0400

    Update HomeScreen.java

--- core/src/com/mygdx/game/views/HomeScreen.java
+++ core/src/com/mygdx/game/views/HomeScreen.java
@@ -28,8 +28,7 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.game.Tutor;
 
 public class HomeScreen implements Screen {
-	//innocent comment passing by
-
+	
 	private Tutor parent;
 	private Stage stage;
 	private SpriteBatch batch;
