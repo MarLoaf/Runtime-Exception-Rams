d24a0269dd596e71b51f66fafe7f99437ab223c8
Pchel666 <pchel66694@gmail.com>
2021-04-07 15:01:09 -0400

    removed unnecessary imports

--- core/src/com/mygdx/game/views/ResultsScreen.java
+++ core/src/com/mygdx/game/views/ResultsScreen.java
@@ -20,7 +20,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonValue;
